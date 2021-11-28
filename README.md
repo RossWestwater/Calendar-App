9e674c4cd59e7861abe8d2759c26d7500866bb51
Ross Westwater <ross.westwater@gmail.com>
2021-11-28 10:55:05 -0700

    updated styling to use some of the inbuilt and adjusted styles. Added time based task coloring. updated push to splice to prevent added saved array bugs. Added screen shot and updated README. Cleaned up old code that was not useful.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Calendar-Challenge
+
+## Description
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+This is a simple task tracker meant to be used for standard business hours based on hourly timeblocks. It is built to create a personalized list of things to do based on times that the items should be completed. This was created to allow individual based tracking to increase efficacy via organization and to familiarize myself with Jquery, moment, bootstrap and expand on my JavaScript knowledge. Pre-assignment, I was not too knowledgeable on some of the methods I ended up using. Using parent(), sibling(), and children() really opened my eyes to some alternative ways of pointing to elements and inputs via their relationships when finding that specific object on the page with a document based search would have been difficult. When I created the "btn" and "btnInput" vars, I was able to get them to point to the appropriate values and arrays and textareas, but the input wasn't being pushed. I managed to observe on my own (after too much time...) that the "btn" var was returning a string and not a number that I had initially put in my "if" statements. After eventually updating the "if" statements to look for a string value of the number, it worked! I'm particularly proud of being able to spot this simple, but significant distinction. I also challenged myself to create everything through JS, though I was advised laying out the basic structure in HTML would have been easier. The coding is functional, but somewhat ugly and I have a tutor set up to see what I might do to clean things up.
+
+![Picture of the calendar app after refreshing the browser](/Assets/screen_shots/Calendar_App.png)
+
+## Tests
+One particular issue I was having that I didn't realize until later was that multiple consecutive clicks of the save button was adding more separate listings to the object array that was getting saved. The result was a hodgepodge of extra commas and various inputs strung together. I was able to use the splice() method instead of push() (which was my initial comfort pick) to ensure that there would only be one item in an object relegated to a given time slot no matter how many times the button was clicked.
--- script.js
+++ script.js
@@ -7,52 +7,42 @@ selectionValue = 0;
 
 timeBlock = [
   {
-    id: 0,
     hour: ["8:00AM"],
     tasks: []
   },
   {
-    id: 1,
     hour: ["9:00AM"],
     tasks: []
   },
   {
-    id: 2,
     hour: ["10:00AM"],
     tasks: []
   },
   {
-    id: 3,
     hour: ["11:00AM"],
     tasks: []
   },
   {
-    id: 4,
     hour: ["12:00PM"],
     tasks: []
   },
   {
-    id: 5,
     hour: ["1:00PM"],
     tasks: []
   },
   {
-    id: 6,
     hour: ["2:00PM"],
     tasks: []
   },
   {
-    id: 7,
     hour: ["3:00PM"],
     tasks: []
   },
   {
-    id: 8,
     hour: ["4:00PM"],
     tasks: []
   },
   {
-    id: 9,
     hour: ["5:00PM"],
     tasks: []
   },
@@ -63,7 +53,6 @@ timeBlock = [
   }
 
 var createTimeblock = function () {
-  // testArr = [];
 //   // loop the following
   for (var i = 0; i < timeBlock.length; i++){
 //   // create container
@@ -71,13 +60,13 @@ var createTimeblock = function () {
   .addClass("row justify-content-center timeblock-container");
 //   // create time listing
   var timeblockTime = $("<div>")
-    timeblockTime.addClass("col-2 bg-info text-light text-center p-2")
+    timeblockTime.addClass("col-2 time-block row hour p-2")
     timeblockTime.attr("data-number", i)
     timeblockTime.text(timeBlock[i].hour);
-//   // create div for future editable text to input obligation
+// create div for future editable text to input obligation
   var timeblockTask = $("<div>")
     timeblockTask.attr("data-number", i)
-    .addClass("col-6 bg-dark border-right border-left border-light text-light p-2")
+    .addClass("col-6 border-right border-left border-light text-light p-2")
     var taskInput = $("<textarea>");
     taskInput
     .addClass("text-light col-12 border-0")
@@ -85,66 +74,77 @@ var createTimeblock = function () {
     .attr("id", i)
     taskInput.text(timeBlock[i].tasks)
     timeblockTask.append(taskInput);
-//   // create div to insert save button. test text to visualize div creation.
+// create div to insert save button. test text to visualize div creation.
   var timeblockSaveEl = $("<div>")
-    .addClass("col-2 bg-success p-2 save-div text-center");
+    .addClass("col-2 saveBtn text-center");
     var saveIcon = $("<i>")
-    saveIcon.addClass("fas fa-save fa-5x")
+    saveIcon.addClass("fas fa-save fa-3x")
     .attr("type", "button")
     .attr("data-number", i)
     .on("click", function (event) {
+// target button and get relevant textarea information
       event.target;
       var btn = $(this).attr("data-number");
       var btnInput = $(this).parent().siblings().children().val();
-      console.log(btn);
-      console.log(btnInput);
-      console.log(timeBlock[0].tasks);
+// have textarea details sent to relevant object array
       if(btn === "0"){
-        (timeBlock[0].tasks).push(btnInput);
+        (timeBlock[0].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
-      if(btn === "1"){
-        (timeBlock[1].tasks).push(btnInput);
+      else if(btn === "1"){
+        (timeBlock[1].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "2"){
-        (timeBlock[2].tasks).push(btnInput);
+        (timeBlock[2].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "3"){
-        (timeBlock[3].tasks).push(btnInput);
+        (timeBlock[3].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "4"){
-        (timeBlock[4].tasks).push(btnInput);
+        (timeBlock[4].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "5"){
-        (timeBlock[5].tasks).push(btnInput);
+        (timeBlock[5].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "6"){
-        (timeBlock[6].tasks).push(btnInput);
+        (timeBlock[6].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "7"){
-        (timeBlock[7].tasks).push(btnInput);
+        (timeBlock[7].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "8"){
-        (timeBlock[8].tasks).push(btnInput);
+        (timeBlock[8].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
       else if (btn === "9"){
-        (timeBlock[9].tasks).push(btnInput);
+        (timeBlock[9].tasks).splice(0, 1, btnInput);
         timeBlockSave();
       }
-      console.log(timeBlock[0].tasks);
     });
     timeblockSaveEl.append(saveIcon);
-//   // start stuffing divs into DOM
-//   // container into parent
-//   // other three sections into the container
+// assign CSS dependent on the current time
+    if (moment() >= moment().set("hour", (i + 9))) {
+// apply new class if task is near/over due date
+    $(timeblockTask).addClass("past");
+    }
+    else if (moment() >= moment().set("hour", (i + 8))) {
+ // apply new class if task is near/over due date
+    $(timeblockTask).addClass("present");
+    }
+    else {
+// apply new class if task is near/over due date
+    $(timeblockTask).addClass("future");
+    }
+// start stuffing divs into DOM
+// container into parent
+// other three sections into the container
   containerEl.append(timeblockTime, timeblockTask, timeblockSaveEl);
   $("#timeblock-place").append(containerEl);
   selectionValue++;
